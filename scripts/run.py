d8fbda269398db4ba8a9282095ac156bb3b0245c
alexyskoutnev <alexyskoutnev@gmail.com>
2023-04-08 16:23:25 -0500

    towr run script works with docker

--- scripts/run.py
+++ scripts/run.py
@@ -36,11 +36,10 @@ def setup_enviroment():
     return py_client 
 
 def importRobot(file=URDF, POSE=([0,0,1], (0.0,0.0,0.0,1.0))):
-    
     solo12 = p.loadURDF(URDF, *POSE)
     return solo12
 
-if __name__ == "__main__":
+def simulation():
     type = "plane"
     sim = Simulation(type)
     py_client = sim.setup(type)
@@ -103,3 +102,67 @@ if __name__ == "__main__":
             p.stepSimulation()
             ROBOT.time_step += 1
     p.disconnect()
+
+# if __name__ == "__main__":
+#     type = "plane"
+#     sim = Simulation(type)
+#     py_client = sim.setup(type)
+#     pybullet_interface = PybulletInterface()
+#     planeId = p.loadURDF("plane.urdf")
+#     ROBOT = SOLO12(URDF, cfg, fixed=False)
+#     gait = Gait(ROBOT)
+#     init_phase = False
+#     trot_phase = True
+
+#     if sim_cfg['mode'] == "towr":
+#         csv_file = open(TOWR, 'r', newline='')
+#         reader = csv.reader(csv_file, delimiter=',')
+#         NUM_TIME_STEPS = sum(1 for row in reader)
+#         csv_file = open(TOWR, 'r', newline='')
+#         reader = csv.reader(csv_file, delimiter=',')
+#         _t = np.linspace(0, NUM_TIME_STEPS/HZ, NUM_TIME_STEPS)
+
+#     pos, angle, velocity, angle_velocity , angle,  stepPeriod = pybullet_interface.robostates(ROBOT.robot)
+#     offsets = np.array([0.5, 0.0, 0.0, 0.5])
+#     trot_2_stance_ratio = 0.5
+#     cmd = np.zeros((12, 1))
+#     for i in range (10000):
+#         if sim_cfg['mode'] == "bezier":
+#             pos, angle, velocity, angle_velocity , angle,  stepPeriod = pybullet_interface.robostates(ROBOT.robot)
+#             if init_phase:
+#                 jointTorques, newCmd = ROBOT.default_stance_control(cmd, p.TORQUE_CONTROL)
+#                 p.setJointMotorControlArray(ROBOT.robot, ROBOT.jointidx['idx'], controlMode=p.TORQUE_CONTROL, forces=jointTorques)
+#                 p.stepSimulation()
+#             elif trot_phase:
+#                 gait_traj, newCmd = gait.runTrajectory(velocity, angle, angle_velocity, offsets, stepPeriod, trot_2_stance_ratio)
+#                 joint_ang_FL, joint_vel_FL, joint_toq_FL = ROBOT.control(gait_traj['FL_FOOT'], ROBOT.EE_index['FL_FOOT'], mode=ROBOT.mode)
+#                 ROBOT.setJointControl(ROBOT.jointidx['FL'], ROBOT.mode, joint_ang_FL[0:3])
+#                 joints_ang_FR, joints_vel_FR, joints_toq_FR = ROBOT.control(gait_traj['FR_FOOT'], ROBOT.EE_index['FR_FOOT'], mode=ROBOT.mode)
+#                 ROBOT.setJointControl(ROBOT.jointidx['FR'], ROBOT.mode, joints_ang_FR[3:6])
+#                 joints_ang_HL, joints_vel_HL, joints_toq_HL = ROBOT.control(gait_traj['HL_FOOT'], ROBOT.EE_index['HL_FOOT'], mode=ROBOT.mode)
+#                 ROBOT.setJointControl(ROBOT.jointidx['BL'], ROBOT.mode, joints_ang_HL[6:9])
+#                 joints_ang_HR, joints_vel_HR, joints_toq_HR = ROBOT.control(gait_traj['HR_FOOT'], ROBOT.EE_index['HR_FOOT'], mode=ROBOT.mode)
+#                 ROBOT.setJointControl(ROBOT.jointidx['BR'], ROBOT.mode, joints_ang_HR[9:12])
+#             p.stepSimulation()
+#             ROBOT.time_step += 1
+#         elif sim_cfg['mode'] == "towr":
+#             with open(TOWR, 'r', newline='') as csv_file:
+#                 try: 
+#                     EE_POSE = np.array([float(x) for x in next(reader)])
+#                 except StopIteration:
+#                     break
+#                 print("EE_POSE -> ", EE_POSE)
+#                 towr = {"COM": EE_POSE[0:3], "FL_FOOT": {'P' : EE_POSE[3:6], 'D': np.zeros(3)}, "FR_FOOT": {'P': EE_POSE[6:9], 'D': np.zeros(3)}, 
+#                         "HL_FOOT": {'P': EE_POSE[9:12], 'D' : np.zeros(3)}, "HR_FOOT": {'P': EE_POSE[12:15], 'D': np.zeros(3)}}
+#                 towr = towr_transform(towr)
+#                 joint_ang_FL, joint_vel_FL, joint_toq_FL = ROBOT.control(towr['FL_FOOT'], ROBOT.EE_index['FL_FOOT'], mode=ROBOT.mode)
+#                 ROBOT.setJointControl(ROBOT.jointidx['FL'], ROBOT.mode, joint_ang_FL[0:3])
+#                 joints_ang_FR, joints_vel_FR, joints_toq_FR = ROBOT.control(towr['FR_FOOT'], ROBOT.EE_index['FR_FOOT'], mode=ROBOT.mode)
+#                 ROBOT.setJointControl(ROBOT.jointidx['FR'], ROBOT.mode, joints_ang_FR[3:6])
+#                 joints_ang_HL, joints_vel_HL, joints_toq_HL = ROBOT.control(towr['HL_FOOT'], ROBOT.EE_index['HL_FOOT'], mode=ROBOT.mode)
+#                 ROBOT.setJointControl(ROBOT.jointidx['BL'], ROBOT.mode, joints_ang_HL[6:9])
+#                 joints_ang_HR, joints_vel_HR, joints_toq_HR = ROBOT.control(towr['HR_FOOT'], ROBOT.EE_index['HR_FOOT'], mode=ROBOT.mode)
+#                 ROBOT.setJointControl(ROBOT.jointidx['BR'], ROBOT.mode, joints_ang_HR[9:12])
+#             p.stepSimulation()
+#             ROBOT.time_step += 1
+#     p.disconnect()
--- scripts/towr_run.py
+++ scripts/towr_run.py
@@ -1,14 +1,73 @@
 #! /usr/bin/python3
 
+import time
 import subprocess
 import shlex
+import argparse
 
+import run
 
-TOWR_SCRIPT = shlex.split("./test 2")
+scripts =  {'copy': 'docker cp <id>:/root/catkin_ws/src/towr/towr/build/traj.csv ./data/traj/towr.csv',
+            'run': 'docker exec <id> ./towr-example',
+            'info': 'docker ps -f ancestor=towr'}
+
+def strip(x):
+    st = " "
+    for s in x:
+        if s == "[" or s == "]":
+            st += ''
+        else:
+            st += s
+    return st
+
+def DockerInfo():
+    p = subprocess.run([scripts['info']], shell=True, capture_output=True, text=True)
+    output = p.stdout.replace('\n', ' ')
+    dockerid, idx = output.split(), output.split().index('towr') - 1
+    return dockerid[idx]
+
+def parse_scripts(scripts_dic, docker_id):
+    for script_name, script in scripts_dic.items():
+        scripts_dic[script_name] = script.replace("<id>", docker_id)
+    return scripts_dic
+        
+def _update(args):
+    """
+    Threaded towr trajectory update function
+    """
+    TOWR_SCRIPT = shlex.split(args['scripts']['run'] + strip(str(args['start_pos'])))
+    p = subprocess.run(TOWR_SCRIPT, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    if p.returncode == 0:
+        print("TOWR found a trajectory")
+        p = subprocess.run(shlex.split(scripts['copy'])) #copy trajectory to simulator data
+        if p.returncode == 0:
+            run.simulation()
+        else: 
+            print("Error in copying Towr Trajectory")
+    else:
+        print("Error in copying Towr Trajectory")
 
 def _run(args):
-    subprocess.run(TOWR_SCRIPT)
+    towr_runtime_0 = time.time()
+    TOWR_SCRIPT = shlex.split(args['scripts']['run'] + strip(str(args['start_pos'])))
+    p = subprocess.run(TOWR_SCRIPT, stdout=subprocess.DEVNULL, stderr=subprocess.STDOUT)
+    towr_runtime_1 = time.time()
+    print(f'TOWR Execution time: {towr_runtime_1 - towr_runtime_0} seconds')
+    if p.returncode == 0:
+        print("TOWR found a trajectory")
+        p = subprocess.run(shlex.split(scripts['copy'])) #copy trajectory to simulator data
+        if p.returncode == 0:
+            run.simulation()
+        else: 
+            print("Error in copying Towr Trajectory")
+    else:
+        print("Error in TOWR")
+        return
 
 if __name__ == "__main__":
-    args = {}
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--d_pos', nargs=3, type=float, default=[1,0,0.3])
+    p_args = parser.parse_args()
+    docker_id = DockerInfo()
+    args = {"start_pos": p_args.d_pos, "docker_id": docker_id, "scripts": parse_scripts(scripts, docker_id)}
     _run(args)
