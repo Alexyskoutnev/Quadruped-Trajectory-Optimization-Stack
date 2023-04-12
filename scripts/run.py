729a559a8fdc90bdc1bbca94364d6c31c81de22d
alexyskoutnev <alexyskoutnev@gmail.com>
2023-04-11 22:00:48 -0500

    added tracking in simulation

--- scripts/run.py
+++ scripts/run.py
@@ -24,6 +24,7 @@ config_sim = "./data/config/simulation.yml"
 cfg = yaml.safe_load(open(config, 'r'))
 sim_cfg = yaml.safe_load(open(config_sim, 'r'))
 TOWR = "./data/traj/towr.csv"
+TRACK = "./data/traj/traj_thirdparty/jointStates.csv"
 HZ = sim_cfg['HZ']
 
 def setup_enviroment():
@@ -56,6 +57,9 @@ def simulation():
         csv_file = open(TOWR, 'r', newline='')
         reader = csv.reader(csv_file, delimiter=',')
         _t = np.linspace(0, NUM_TIME_STEPS/HZ, NUM_TIME_STEPS)
+    elif sim_cfg['mode'] == "track":
+        csv_file = open(TRACK, 'r', newline='')
+        reader =csv.reader(csv_file, delimiter=' ')
 
     pos, angle, velocity, angle_velocity , angle,  stepPeriod = pybullet_interface.robostates(ROBOT.robot)
     offsets = np.array([0.5, 0.0, 0.0, 0.5])
@@ -81,26 +85,41 @@ def simulation():
             p.stepSimulation()
             ROBOT.time_step += 1
         elif sim_cfg['mode'] == "towr":
-            with open(TOWR, 'r', newline='') as csv_file:
-                try: 
-                    EE_POSE = np.array([float(x) for x in next(reader)])
-                except StopIteration:
-                    break
-                print("EE_POSE -> ", EE_POSE)
-                towr = {"COM": EE_POSE[0:3], "FL_FOOT": {'P' : EE_POSE[3:6], 'D': np.zeros(3)}, "FR_FOOT": {'P': EE_POSE[6:9], 'D': np.zeros(3)}, 
-                        "HL_FOOT": {'P': EE_POSE[9:12], 'D' : np.zeros(3)}, "HR_FOOT": {'P': EE_POSE[12:15], 'D': np.zeros(3)}}
-                towr = towr_transform(towr)
-                joint_ang_FL, joint_vel_FL, joint_toq_FL = ROBOT.control(towr['FL_FOOT'], ROBOT.EE_index['FL_FOOT'], mode=ROBOT.mode)
-                ROBOT.setJointControl(ROBOT.jointidx['FL'], ROBOT.mode, joint_ang_FL[0:3])
-                joints_ang_FR, joints_vel_FR, joints_toq_FR = ROBOT.control(towr['FR_FOOT'], ROBOT.EE_index['FR_FOOT'], mode=ROBOT.mode)
-                ROBOT.setJointControl(ROBOT.jointidx['FR'], ROBOT.mode, joints_ang_FR[3:6])
-                joints_ang_HL, joints_vel_HL, joints_toq_HL = ROBOT.control(towr['HL_FOOT'], ROBOT.EE_index['HL_FOOT'], mode=ROBOT.mode)
-                ROBOT.setJointControl(ROBOT.jointidx['BL'], ROBOT.mode, joints_ang_HL[6:9])
-                joints_ang_HR, joints_vel_HR, joints_toq_HR = ROBOT.control(towr['HR_FOOT'], ROBOT.EE_index['HR_FOOT'], mode=ROBOT.mode)
-                ROBOT.setJointControl(ROBOT.jointidx['BR'], ROBOT.mode, joints_ang_HR[9:12])
+            try: 
+                EE_POSE = np.array([float(x) for x in next(reader)])
+            except StopIteration:
+                break
+            towr = {"COM": EE_POSE[0:3], "FL_FOOT": {'P' : EE_POSE[3:6], 'D': np.zeros(3)}, "FR_FOOT": {'P': EE_POSE[6:9], 'D': np.zeros(3)}, 
+                    "HL_FOOT": {'P': EE_POSE[9:12], 'D' : np.zeros(3)}, "HR_FOOT": {'P': EE_POSE[12:15], 'D': np.zeros(3)}}
+            towr = towr_transform(towr)
+            joint_ang_FL, joint_vel_FL, joint_toq_FL = ROBOT.control(towr['FL_FOOT'], ROBOT.EE_index['FL_FOOT'], mode=ROBOT.mode)
+            ROBOT.setJointControl(ROBOT.jointidx['FL'], ROBOT.mode, joint_ang_FL[0:3])
+            joints_ang_FR, joints_vel_FR, joints_toq_FR = ROBOT.control(towr['FR_FOOT'], ROBOT.EE_index['FR_FOOT'], mode=ROBOT.mode)
+            ROBOT.setJointControl(ROBOT.jointidx['FR'], ROBOT.mode, joints_ang_FR[3:6])
+            joints_ang_HL, joints_vel_HL, joints_toq_HL = ROBOT.control(towr['HL_FOOT'], ROBOT.EE_index['HL_FOOT'], mode=ROBOT.mode)
+            ROBOT.setJointControl(ROBOT.jointidx['BL'], ROBOT.mode, joints_ang_HL[6:9])
+            joints_ang_HR, joints_vel_HR, joints_toq_HR = ROBOT.control(towr['HR_FOOT'], ROBOT.EE_index['HR_FOOT'], mode=ROBOT.mode)
+            ROBOT.setJointControl(ROBOT.jointidx['BR'], ROBOT.mode, joints_ang_HR[9:12])
+            p.stepSimulation()
+            ROBOT.time_step += 1
+        elif sim_cfg['mode'] == "track":
+            try:
+                joints = np.array([float(x) for x in next(reader)])[1:]
+            except StopIteration:
+                break
+            revoluteJointIndices = [0, 1, 2, 4, 5, 6, 8, 9, 10, 12, 13, 14]
+            maxForces = np.ones(12)*20
+            posGains = np.ones(12)*1.0
+            p.setJointMotorControlArray(ROBOT.robot, revoluteJointIndices, 
+                                    controlMode=p.POSITION_CONTROL, targetPositions=joints, forces=maxForces, positionGains=posGains)   
             p.stepSimulation()
             ROBOT.time_step += 1
     p.disconnect()
+
+
+if __name__ == "__main__":
+    simulation()
+
 
 # if __name__ == "__main__":
 #     type = "plane"
