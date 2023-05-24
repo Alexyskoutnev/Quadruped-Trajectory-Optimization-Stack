reworked the mpc loop to lookahead fixed time interval and made the code more compute efficent (removed unnecessary checks/calls)

SOLO12_SIM_CONTROL/mpc.py
@@ -1,57 +1,97 @@
 import csv
 import math
+import threading
+import time
 
 import numpy as np
 import pandas as pd
 
 import SOLO12_SIM_CONTROL.config.global_cfg as global_cfg
-from SOLO12_SIM_CONTROL.utils import zero_filter, percentage_look_ahead
+from SOLO12_SIM_CONTROL.utils import zero_filter, percentage_look_ahead, look_ahead
+
+
+class MPC_THREAD(threading.Thread):
+    def __init__(self, obj) -> None:
+        threading.Thread.__init__(self)
+        self.obj = obj
+
+    def run(self):
+        while True:
+            self.obj.update()
+            time.sleep(0.01)
 
 class MPC(object):
 
-    def __init__(self, args, current_traj, new_traj) -> None:
+    def __init__(self, args, current_traj, new_traj, lookahead=None, hz=100) -> None:
         self.args = args
         self.current_traj = current_traj
         self.current_traj_temp = "/tmp/towr_old_temp.csv"
         self.new_traj = new_traj
         self.cutoff_idx = 0
+        self.last_timestep = 0.0
+        self.lookahead = lookahead if lookahead else 600
+        self.next_traj_step = 0
+        self.hz = hz
     
     def combine(self):
         """Combines the old and new csv trajectories together
         """
+        time_combine = time.process_time()
         _old_traj = open(self.current_traj, "r")
         _new_traj = open(self.new_traj, "r")
-        self._truncate_csv(_old_traj, self.args['look_ahead'])
-        df_old = pd.read_csv(self.current_traj_temp).to_numpy()
+        # print("current RUN STEP1-> ", self.cutoff_idx)
+        # print("current runtime1-> ",  self.last_timestep)
+        df_old = self._truncate_csv(_old_traj).to_numpy()
         df_new = pd.read_csv(self.new_traj).to_numpy()
         combined_df = np.concatenate((df_old, df_new), axis=0)
         combined_df = pd.DataFrame(combined_df)
         combined_df.to_csv(self.new_traj, index=False, header=None)
+        # print("current RUN STEP2-> ", self.cutoff_idx)
+        # print("current runtime2-> ",  self.last_timestep)
+        # print(f"Combined time -> {time.process_time() - time_combine}")
+        
+        
+        
+        # breakpoint()
 
     def plan(self, args):
         """
-        Trajcetory plan towards the final goal
+        Trajectory plan towards the final goal
+
+        Args: 
+            args (dic) : updates the input arguments to Towr script
+        Returns:
+            self.args (dic) : input argument to Towr script
         """
         self._step()
-        _state_dic = self._state(self.args['look_ahead'])
+        _state_dic = self._state()
         self.args['-s'] = _state_dic["CoM"]
         self.args['-s_ang'] = _state_dic['orientation']
         self.args['-e1'] = _state_dic["FL_FOOT"]
         self.args['-e2'] = _state_dic["FR_FOOT"]
         self.args['-e3'] = _state_dic["HL_FOOT"]
         self.args['-e4'] = _state_dic["HR_FOOT"]
+        self.args['-t'] = global_cfg.ROBOT_CFG.runtime + (self.lookahead / self.hz)
         print("ARRGS", self.args)
         return self.args
 
     def update(self):
-        """Updates the state of the MPC loop
+        """
+        Updates the state of the MPC loop
         """
         self.cutoff_idx = global_cfg.RUN.step
+        self.last_timestep = global_cfg.ROBOT_CFG.runtime
+
+    def update_timestep(self):
+        """Update the timestep by one time unit
+        """
+        self.last_timestep += 1/self.hz
 
     def _step(self):
-        """Updates step vector toward robot's goal depending on the stepping size
         """
-        step_size = self.args['step_size'] #try implementing in config-file
+        Updates step vector toward robot's goal depending on the stepping size
+        """
+        step_size = self.args['step_size']
         global_pos = np.array(global_cfg.ROBOT_CFG.linkWorldPosition)
         goal = global_cfg.ROBOT_CFG.robot_goal
         diff_vec = np.clip(goal - global_pos, -step_size, step_size)
@@ -59,20 +99,23 @@ class MPC(object):
         self.args['-g'] = list(global_pos + diff_vec)
         self.args['-g'][2] = 0.24
 
-    def _state(self, p=0.60):
+    def _state(self):
         """Returns the updated robot state back to the optimizer
 
         Args:
-            p (float, optional): Percentage to look-ahead in trajectory. Defaults to 0.60.
-
+            lookahead_steps (int): number of steps to look ahead for the 
+                                   next trajectory start
         Returns:
-            _type_: robot state
+            state (dic): robot state dic
         """
         state = {"CoM": None, "orientation": None, "FL_FOOT": None, 
              "FR_FOOT": None, "HL_FOOT": None, "HR_FOOT": None}
         with open(self.current_traj, "r", newline='') as f:
-            reader = percentage_look_ahead(f, p)
-            row = next(reader)
+            # global_cfg.RUN._wait = True
+            # breakpoint()
+            # global_cfg.RUN._wait = False
+            reader, step = look_ahead(f, self.last_timestep, self.lookahead)
+            row = next(reader)[1:]
             state["CoM"] = [float(_) for _ in row[0:3]]
             state["orientation"] = [float(_) for _ in row[3:6]]
             state["FL_FOOT"] = [float(_) for _ in row[6:9]]
@@ -80,19 +123,19 @@ class MPC(object):
             state["HL_FOOT"] = [float(_) for _ in row[12:15]]
             state["HR_FOOT"] = [float(_) for _ in row[15:18]]
         state = {key: zero_filter(value) for key, value in state.items()}
+        self.next_traj_step = step + self.lookahead - 1
         return state
 
-    def _truncate_csv(self, file : object, lookahead=0.60):
+    def _truncate_csv(self, file : object):
         """Predicts where robot is in simulation and truncates old trajectory points
 
         Args:
-            file (_type_): csv trajectory file
+            file (object): csv trajectory file
             lookahead (float): upper bound cutoff for old trajectory
         """
         df = pd.read_csv(file)
-        num_row = len(df)
-        start_idx = self.cutoff_idx
-        end_idx = math.ceil(num_row * lookahead)
+        start_idx = self.cutoff_idx - 1
+        end_idx = self.next_traj_step
         _new_csv = df.iloc[start_idx:end_idx]
-        _new_csv.to_csv(self.current_traj_temp, index=False)
+        return _new_csv
 

SOLO12_SIM_CONTROL/robot/robot.py
@@ -78,6 +78,7 @@ class SOLO12(object):
         self._joint_ang = None
         self._joint_vel = None
         self._joint_toq = None
+        self._time_step = config['timestep']
 
     def CoM_states(self):
         CoM_pos, CoM_angle = p.getBasePositionAndOrientation(self.robot)
@@ -93,6 +94,19 @@ class SOLO12(object):
     @property
     def jointangles(self):
         return self._joint_ang
+
+    @property
+    def jointstate(self):
+        return {'q_cmd': self._joint_ang, "q_vel": self._joint_vel, "q_toq": self._joint_toq}
+
+    @property
+    def time(self):
+        """Internal run time for robot based on simulation frequency
+
+        Returns:
+            float: runtime since start of simulation
+        """
+        return self.time_step * self._time_step
 
     def setJointControl(self, jointsInx, controlMode, cmd_pose, cmd_vel=None, cmd_f=None):
         if 'P' == controlMode or 'PD' == controlMode:
@@ -162,16 +176,15 @@ class SOLO12(object):
         q_mes[:] = [state[0] for state in jointStates]
         v_mes[:] = [state[1] for state in jointStates]
         q_toq = self._motor.convert_to_torque(q_cmd, q_mes, v_mes)
+        self._joint_toq = q_toq
         return q_cmd, q_vel, q_toq
         
     def inv_kinematics_multi(self, cmds, indices, mode = 'P'):
         assert(len(indices) == 4)
-        breakpoint()
         joint_position = np.zeros(12)
         joint_velocity = np.zeros(12)
         idx_2_EE = {3 : "FL_FOOT", 7: "FR_FOOT", 11: "HL_FOOT", 15: "HR_FOOT"}
         for idx in indices:
-            breakpoint()
             if idx_2_EE[idx] == "FL_FOOT":
                 joint_position[0:3] = self.inv_kinematics(cmds[idx_2_EE[idx]], idx, mode)[0][0:3]
             elif idx_2_EE[idx] == "FR_FOOT":

scripts/run.py
@@ -55,6 +55,9 @@ def _global_update(ROBOT, kwargs):
     global_cfg.ROBOT_CFG.EE['FR_FOOT'] = list(kwargs['FR_FOOT'])
     global_cfg.ROBOT_CFG.EE['HL_FOOT'] = list(kwargs['HL_FOOT'])
     global_cfg.ROBOT_CFG.EE['HR_FOOT'] = list(kwargs['HR_FOOT'])
+    global_cfg.ROBOT_CFG.runtime = ROBOT.time
+    global_cfg.RUN.step += 1
+    global_cfg.ROBOT_CFG.joint_state = ROBOT.jointstate
 
 def keypress():
     global key_press_init_phase
@@ -108,7 +111,7 @@ def simulation():
                 _, _, joint_toq = ROBOT.default_stance_control()
                 p.setJointMotorControlArray(ROBOT.robot, ROBOT.jointidx['idx'], controlMode=p.TORQUE_CONTROL, forces=joint_toq)
                 p.stepSimulation()
-    for i in range (10000):
+    for i in range (100000):
         if sim_cfg['mode'] == "bezier":
             if sim_cfg['py_interface']:
                 pos, angle, velocity, angle_velocity , angle,  stepPeriod = pybullet_interface.robostates(ROBOT.robot)
@@ -136,24 +139,32 @@ def simulation():
 
             p.stepSimulation()
             ROBOT.time_step += 1
+
         elif sim_cfg['mode'] == "towr":
             try:
-                if global_cfg.RUN.update:
+                if global_cfg.RUN._wait: #Waits for towr thread to copy over the trajectory
+                    # print("============WAIT STATE============")
+                    # print(f'i: {i}')
+                    time.sleep(0.001)
+                    continue
+                elif global_cfg.RUN._update:
+                    print("============UPDATE STATE============")
                     mutex.acquire()
                     print("==========Reading updated CSV==========")
-                    reader = nearestPoint(global_cfg.ROBOT_CFG.last_POSE, open(TOWR, 'r', newline=''))
+                    # reader = nearestPoint(global_cfg.ROBOT_CFG.last_POSE, open(TOWR, 'r', newline=''))
+                    reader = csv.reader(open(TOWR, 'r', newline=''))
                     mutex.release()
-                    global_cfg.RUN.update = False 
+                    global_cfg.RUN._update = False 
                     global_cfg.RUN.step = 0
-                EE_POSE = np.array([float(x) for x in next(reader)])
+                    # breakpoint()
+                EE_POSE = np.array([float(x) for x in next(reader)])[1:]
+                # print(f"EE-> {EE_POSE}")
                 global_cfg.ROBOT_CFG.last_POSE = EE_POSE[0:3]
                 global_cfg.RUN.TOWR_POS = EE_POSE[0:3]
                 towr = towr_transform(ROBOT, vec_to_cmd_pose(EE_POSE))
             except StopIteration:
-                # print("stance ->")
                 log.write("==========STANCE==========")
-                global_cfg.RUN._wait = True
-                last_cnt = ROBOT.time_step
+                global_cfg.RUN._stance = True
             
             ## Logging ##
             log.write(f"TIME STEP ==> {global_cfg.RUN.step}\n")
@@ -162,7 +173,7 @@ def simulation():
             log.write(f"=========Global Vars=========\n")
             log.write(f"{global_cfg.print_vars(log.log)}\n")
 
-            if global_cfg.RUN._wait:
+            if global_cfg.RUN._stance:
                 _, _, joint_toq = ROBOT.default_stance_control(ROBOT.q_init, p.TORQUE_CONTROL)
                 p.setJointMotorControlArray(ROBOT.robot, ROBOT.jointidx['idx'], controlMode=p.TORQUE_CONTROL, forces=joint_toq)
             else:
@@ -186,10 +197,10 @@ def simulation():
                     ROBOT.setJointControl(ROBOT.jointidx['BR'], ROBOT.mode, joints_ang_HR[9:12])
                 elif ROBOT.mode == 'torque':
                     ROBOT.setJointControl(ROBOT.jointidx['BR'], ROBOT.mode, joints_toq_HR[9:12])
-                global_cfg.RUN.step += 1
+
             p.stepSimulation()
-            _global_update(ROBOT, ROBOT.state)
             ROBOT.time_step += 1
+            _global_update(ROBOT, ROBOT.state)
         elif sim_cfg['mode'] == "track":
             try:
                 joints = np.array([float(x) for x in next(reader)])[3:]
@@ -224,7 +235,6 @@ def simulation():
             except StopIteration:
                 break
             cmd = trajectory_2_world_frame(ROBOT,create_cmd(ee_pose))
-            time.sleep(0.01)
             joint_ang_FL, joint_vel_FL, joint_toq_FL = ROBOT.control(cmd['FL_FOOT'], ROBOT.EE_index['FL_FOOT'], mode=ROBOT.mode)
             if ROBOT.mode == 'P' or ROBOT.mode == 'PD':
                 ROBOT.setJointControl(ROBOT.jointidx['FL'], ROBOT.mode, joint_ang_FL[0:3])